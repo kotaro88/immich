3b478158e60799a4972c7bc90276ab6d3a277f22
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-08-07 19:10:38 +0530

    reset sync stream after switching to beta timeline

--- mobile/lib/pages/common/change_experience.page.dart
+++ mobile/lib/pages/common/change_experience.page.dart
@@ -60,6 +60,7 @@ class _ChangeExperiencePageState extends ConsumerState<ChangeExperiencePage> {
       ref.read(websocketProvider.notifier).startListeningToBetaEvents();
 
       await ref.read(driftProvider).reset();
+      await Store.put(StoreKey.shouldResetSync, true);
       final permission = await ref.read(galleryPermissionNotifier.notifier).requestGalleryPermission();
 
       if (permission.isGranted) {
